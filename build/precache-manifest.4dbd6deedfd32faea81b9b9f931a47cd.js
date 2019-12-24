6cc54f8ef0c489c32fb89120cbc7e919518c47a7
System Administrator <root@1111.local>
2019-12-24 15:33:20 -0500

    make it better

---
+++ build/precache-manifest.4dbd6deedfd32faea81b9b9f931a47cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ba13da2f7f02b91b18ef26fe737fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3db6eec01b25976aae76",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2bfc03852665f0e66615",
+    "url": "/static/js/2.82953d8f.chunk.js"
+  },
+  {
+    "revision": "10f789042a0b49ed82dea37940bac9f4",
+    "url": "/static/js/2.82953d8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3db6eec01b25976aae76",
+    "url": "/static/js/main.5f048e39.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
